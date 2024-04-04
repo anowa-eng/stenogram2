58e15ef0c6d8ce14a4cd0a438485a0b7bff4e3fa
anova02 <georgemorrissette2010@gmail.com>
2024-04-03 19:54:50 -0700

    feat: Add classes

--- classes.py
+++ classes.py
@@ -1,6 +1,12 @@
 from abc import abstractmethod
 from typing import Dict
 from cv2.typing import Point, MatLike
+from eng_syl import Syllabel
+from pysle import isletool
+from hyphen import Hyphenator
+
+_isle = isletool.Isle()
+_hyphenator = Hyphenator()
 
 class PolylinesGlyphPath:
     def __init__(self, points: list[Point]):
@@ -12,6 +18,11 @@ class PolylinesGlyph:
         self.joining_point = joining_point or path[-1]
 
 class Shorthand:
+    @abstractmethod
+    def convert(string: str):
+        pass
+
+class WrittenShorthand(Shorthand):
     @property
     @abstractmethod
     def glyphs() -> Dict[str, PolylinesGlyph]:
@@ -19,3 +30,14 @@ class Shorthand:
     @abstractmethod
     def convert(string: str) -> MatLike:
         pass
+
+class TypableShorthand(Shorthand):
+    @abstractmethod
+    def convert(string: str) -> str:
+        pass
+
+class Word:
+    def __init__(self, word: str):
+        self.word = word
+        self.syllables = _hyphenator.syllables(word)
+        self.pronunciations = _isle.lookup(word)
