398837058444448f34daeacb92be6279f6cfa450
anova02 <georgemorrissette2010@gmail.com>
2024-04-04 19:28:20 -0700

    Add to base classes

--- classes.py
+++ classes.py
@@ -1,11 +1,11 @@
-from abc import abstractmethod
-from typing import Dict
+import abc
+from typing import Dict, Generator, List, Tuple, Optional
 from cv2.typing import Point, MatLike
 from pysle import isletool
-from hyphen import Hyphenator
+from pyphen import Pyphen
+import helpers
 
 _isle = isletool.Isle()
-_hyphenator = Hyphenator()
 
 class PolylinesGlyphPath:
     def __init__(self, points: list[Point]):
@@ -16,27 +16,39 @@ class PolylinesGlyph:
         self.path = path
         self.joining_point = joining_point or path[-1]
 
-class Shorthand:
-    @abstractmethod
+class Word:
+    def __init__(self, word: str, language: Optional[str] = None):
+        self.__pyphen = Pyphen(lang=language)
+        self.word = word
+        self.__syllables = self.__pyphen.inserted(word, '/').split('/')
+        print(self.__syllables)
+        self.pronunciations = [i.toList()[0] for i in _isle.lookup(word)]
+    def syllables(self, pronunciation: int = 0):
+        ipa = self.pronunciations[pronunciation]
+        syllables = zip(self.__syllables, ipa)
+        for syllable in syllables:
+            yield syllable
+
+for s in Word("project", "en-US").syllables(2):
+    print(s)
+
+class Shorthand(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def briefs() -> Dict[str, List[str]]:
+        pass
+    @abc.abstractmethod
+    def build_word(word: Word):
+        pass
+    @abc.abstractmethod
     def convert(string: str):
         pass
 
 class WrittenShorthand(Shorthand):
     @property
-    @abstractmethod
+    @abc.abstractmethod
     def glyphs() -> Dict[str, PolylinesGlyph]:
         pass
-    @abstractmethod
+    @abc.abstractmethod
     def convert(string: str) -> MatLike:
         pass
-
-class TypableShorthand(Shorthand):
-    @abstractmethod
-    def convert(string: str) -> str:
-        pass
-
-class Word:
-    def __init__(self, word: str):
-        self.word = word
-        self.syllables = _hyphenator.syllables(word)
-        self.pronunciations = _isle.lookup(word)
